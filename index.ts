dde15069a1eebd362d05844b1ed668385e5412da
Johnie Hjelm <johnie.hjelm@schibsted.com>
2025-08-08 16:11:21 +0200

    fix: standardize quote style in index.ts exports

--- index.ts
+++ index.ts
@@ -1,2 +1,2 @@
-export * from "@slack/web-api/dist/types/request/index";
-export * from "@slack/web-api/dist/types/response/index";
+export * from '@slack/web-api/dist/types/request/index';
+export * from '@slack/web-api/dist/types/response/index';
